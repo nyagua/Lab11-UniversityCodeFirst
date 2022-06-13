8e8dfa36aa6186832350b2fcfe29560e3ba864f5
nyagua <nelson.yagua@tecsup.edu.pe>
2022-06-12 22:46:03 -0500

    Agregar archivos de proyecto.

---
+++ Prueba1/Migrations/202206130306299_v2.cs
@@ -0,0 +1,20 @@
+﻿namespace Prueba1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.People", "IsActive");
+            DropColumn("dbo.OfficeAssignments", "IsActive");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.OfficeAssignments", "IsActive", c => c.Boolean());
+            AddColumn("dbo.People", "IsActive", c => c.Boolean());
+        }
+    }
+}
